eac42c9585a061e99bd567dc367a1e7cb1e21a58
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-30 10:08:29 +0300

    adds the clsx package, adds the hero and our story section on the home page, adds a constData folder

---
+++ src/components/homePage/HeroSection.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../ui/button";
+
+
+export default function HeroSection() {
+  return (
+    <>
+        <section className="bg-slate-500 h-[90vh]">
+            <div className="grid grid-cols-12 items-end  h-full px-4">
+            <div className="col-start-9 col-end-12 pb-96 space-y-4 text-white">
+                <h1 className="font-bold text-3xl">Opening Doors to Inclusive Journeys</h1>
+                <p className="c">Because everyone should have access to great travel experiences</p>
+                <Button size='lg' className="bg-green-600 hover:bg-green-700">
+                    Get Involved
+                </Button>
+
+            </div>
+            </div>
+        </section>
+    </>
+  )
+}
---
+++ src/components/homePage/OurStorySection.tsx
@@ -0,0 +1,70 @@
+import { ourStorySectionEntries } from "@/constData/ourStory";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+export default function OurStorySection() {
+  return (
+    <>
+      <section className="max-w-7xl mx-auto">
+        <div className="bg-white rounded-lg px-4 -mt-20">
+          <div className="flex  w-full justify-center gap-4">
+            {ourStorySectionEntries.map((entry) => (
+              <div key={entry.id} className="">
+                <div className="flex h-20 gap-2 items-center">
+                  {/* <p className="font-bold text-4xl"> */}
+                  <p className={cn("font-extrabold text-4xl", entry.color)}>
+                    {entry.figure}
+                    <span className="">{entry.symbol}</span>
+                  </p>
+                  <p className="text-zinc-600">{entry.details}</p>
+                  <Separator
+                    orientation="vertical"
+                    className={cn(entry.id === 3 && "bg-transparent")}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bg-amber-900 rounded-lg">
+            <div className="flex flex-col justify-start space-y-4 text-white px-4 py-8">
+              <h2 className="font-thin ">Our story</h2>
+              <div className="border rounded-lg w-6/12 space-y-4">
+                <h3 className="font-medium text-lg mb-8">
+                  Accessible Tourism: A Path to Inclusion
+                </h3>
+                <p className="c">
+                  Our misiion is promotes inclusive tourism, ensuring travel is
+                  accessible to all. With 1.3 billion people (16% of the global
+                  population) having disabilities, most tourism facilities
+                  remain inaccessible.
+                </p>
+                <p className="c">
+                  We collaborate with policymakers, industry leaders, and
+                  communities to create barrier-free travel. Through advocacy,
+                  training, and innovation, we champion equity and dignity for
+                  all travelers.
+                </p>
+                <p className="c">
+                  {" "}
+                  Investing in inclusive tourism enriches experiences, benefits
+                  communities, and transforms the industry into a model of
+                  inclusivity.
+                </p>
+                <p className="c">Together, we make travel barrier-free.</p>
+              </div>
+              <Button
+                variant="outline"
+                size="default"
+                className="text-green-600 bg-transparent font-medium border-green-600 w-48"
+              >
+                Learn More...
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
