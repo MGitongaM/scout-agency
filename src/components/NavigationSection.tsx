7b50b8edacdb4859e90803b6d8f93d2bdda977d4
Michael Gitonga <gitoshmbae@gmail.com>
2025-02-01 13:45:04 +0300

    add the mobile navigation section on src/components/NavigationSection.tsx , adds sheet component from shadcn ui

--- src/components/NavigationSection.tsx
+++ src/components/NavigationSection.tsx
@@ -7,13 +7,15 @@ import {
 import Link from "next/link";
 import { Button } from "./ui/button";
 import CloudinaryImage from "./CloudinaryImage";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 export default function NavigationSection() {
   return (
     <>
-      <nav className="bg-black/50 text-white w-full px-4 ">
+      <nav className="bg-black/95 backdrop-blur-lg text-white w-full px-4  z-10">
         <NavigationMenu className="">
-          <NavigationMenuList className="w-screen flex justify-around px-8">
+          <NavigationMenuList className="w-screen flex justify-between md:justify-around px-8">
             <NavigationMenuItem>
               <Link href="/">
                 <CloudinaryImage
@@ -25,18 +27,42 @@ export default function NavigationSection() {
                 />
               </Link>
             </NavigationMenuItem>
-            <div className="w-[50vw] mx-auto flex justify-evenly gap-x-4">
-              {navigationEntries.map((entry) => (
-                <NavigationMenuItem key={entry.id}>
-                  <Link href={entry.link}>{entry.title}</Link>
-                </NavigationMenuItem>
-              ))}
+            <div className="hidden md:flex items-center">
+              <div className="w-[50vw] mx-auto flex justify-end gap-x-4 lg:gap-x-12 px-4">
+                {navigationEntries.map((entry) => (
+                  <NavigationMenuItem key={entry.id}>
+                    <Link href={entry.link}>{entry.title}</Link>
+                  </NavigationMenuItem>
+                ))}
+              </div>
+              <NavigationMenuItem>
+                <Button size="lg" className="bg-green-600 hover:bg-green-700">
+                  Donate
+                </Button>
+              </NavigationMenuItem>
             </div>
-            <NavigationMenuItem>
-              <Button size="lg" className="bg-green-600 hover:bg-green-700">
-                Donate
-              </Button>
-            </NavigationMenuItem>
+          {/* mobile nav  */}
+          <div className="block md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Menu size={48} className="" />
+              </SheetTrigger>
+              <SheetContent side='top'>
+                <div className="flex flex-col justify gap-x-4">
+                  {navigationEntries.map((entry) => (
+                    <NavigationMenuItem key={entry.id}>
+                      <Link href={entry.link}>{entry.title}</Link>
+                    </NavigationMenuItem>
+                  ))}
+                </div>
+                <NavigationMenuItem>
+                  <Button size="lg" className="bg-green-600 hover:bg-green-700">
+                    Donate
+                  </Button>
+                </NavigationMenuItem>
+              </SheetContent>
+            </Sheet>
+          </div>
           </NavigationMenuList>
         </NavigationMenu>
       </nav>
--- src/components/homePage/HeroSection.tsx
+++ src/components/homePage/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
   return (
     <>
       
-      <section className=" h-[90vh]">
+      <section className=" h-[95vh]">
         <div className="grid grid-cols-12 items-end relative h-full px-4">
           <CloudinaryImage
             src="scout_home_hero_pe8hf5"
@@ -14,7 +14,7 @@ export default function HeroSection() {
             alt="scout group agency home hero img"
             className="object-cover w-full h-full absolute top-0 left-0 -z-30"
           />
-          <div className="bg-slate-900/50 h-[90vh] w-screen absolute top-0 left-0 -z-30"></div>
+          <div className="bg-slate-900/50 h-[95vh] w-screen absolute top-0 left-0 -z-30"></div>
           <div className=" absolute top-72 left-0 -z-20 col-start-2 col-end-11 md:col-start-7 md:col-end-12 lg:col-start-8 lg:col-end-12 pb-96 space-y-4 text-white">
             <h1 className="font-bold text-3xl">
               Opening Doors to Inclusive Journeys
