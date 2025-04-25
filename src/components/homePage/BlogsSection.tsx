9c6c6c610442d9f064086825b32468f8068f83a6
Michael Gitonga <gitoshmbae@gmail.com>
2025-04-25 15:23:26 +0300

    Updates the semi bg colors with the standand tailwind ones

--- src/components/homePage/BlogsSection.tsx
+++ src/components/homePage/BlogsSection.tsx
@@ -36,7 +36,7 @@ export default function BlogsSection() {
           ))}
         </div>
         <div className="grid place-content-center mt-8">
-          <Button size="default" className="bg-sgaGreen1 text-lg hover:bg-green-700">
+          <Button size="default" className="bg-lime-600 text-lg hover:bg-green-700">
             View More Blogs & Articles
           </Button>
         </div>
