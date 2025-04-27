ae7bf759a078ae8411a28145147d7f96100b7fbc
Michael Gitonga <gitoshmbae@gmail.com>
2025-04-27 18:51:11 +0300

    Adds the Get Involved Page src/app/get-involved/, Adds the sections for the Get Invloved page in src/components/getInvolvedPage/ , Updates the navgation entries for get invloved page in src/constData/homePage.tsx

---
+++ src/components/getInvolvedPage/ChannelsOfGivingSection.tsx
@@ -0,0 +1,85 @@
+import { ArrowRight } from "lucide-react";
+
+export default function ChannelsOfGivingSection() {
+  return (
+    <section className="h-full">
+      <div className="max-w-5xl mx-auto px-4 py-20">
+        <div className="max-w-xl mx-auto text-center space-y-6">
+          <h2 className="text-xl font-thin text-stone-400">
+            Channels Of giving
+          </h2>
+          <p className="text-center leading-8">
+            What matters most is that we choose to act. Join us today and help
+            create a world where every child gets to explore, learn, and belong.
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-evenly items-center gap-12 mt-20">
+          <div className="w-5/12 min-h-[40vh] bg-green-300 rounded-lg bg-[url('https://res.cloudinary.com/dh8qlzbzk/image/upload/v1745753776/zurura-initiative_ActiviesBackground_Image_p0larw.png')] bg-cover bg-center ">
+            <div className="bg-green-700 text-slate-100 rounded-lg px-4 py-3 mt-52 mx-4">
+              <div className="flex justify-evenly items-start gap-2">
+                <div className="c">
+                  <p className=" font-semibold">Sponsor A School</p>
+                  <p className="c">
+                    Help an entire special needs or inclusive school experience
+                    the magic of a Zurura trip.{" "}
+                  </p>
+                </div>
+                <div className="c">
+                  <ArrowRight className="size-10" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-5/12 min-h-[40vh] bg-green-300 rounded-lg bg-[url('https://res.cloudinary.com/dh8qlzbzk/image/upload/v1745753764/zurura-initiative_About_Image_hckw5w.png')] bg-cover bg-center ">
+            <div className="bg-green-700 text-slate-100 rounded-lg px-4 py-3 mt-52 mx-4">
+              <div className="flex justify-evenly items-start gap-2">
+                <div className="c">
+                  <p className=" font-semibold">Sponsor A School</p>
+                  <p className="c">
+                    Help an entire special needs or inclusive school experience
+                    the magic of a Zurura trip.{" "}
+                  </p>
+                </div>
+                <div className="c">
+                  <ArrowRight className="size-10" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-5/12 min-h-[40vh] bg-green-300 rounded-lg bg-[url('https://res.cloudinary.com/dh8qlzbzk/image/upload/v1745753768/zurura-initiative_Hero_Image_cw3ev2.png')] bg-cover bg-center ">
+            <div className="bg-green-700 text-slate-100 rounded-lg px-4 py-3 mt-52 mx-4">
+              <div className="flex justify-evenly items-start gap-2">
+                <div className="c">
+                  <p className=" font-semibold">Sponsor A School</p>
+                  <p className="c">
+                    Help an entire special needs or inclusive school experience
+                    the magic of a Zurura trip.{" "}
+                  </p>
+                </div>
+                <div className="c">
+                  <ArrowRight className="size-10" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-5/12 min-h-[40vh] bg-green-300 rounded-lg bg-[url('https://res.cloudinary.com/dh8qlzbzk/image/upload/v1745753764/zurura-initiative_Object_Image_vdzwxc.png')] bg-cover bg-center ">
+            <div className="bg-green-700 text-slate-100 rounded-lg px-4 py-3 mt-52 mx-4">
+              <div className="flex justify-evenly items-start gap-2">
+                <div className="c">
+                  <p className=" font-semibold">Sponsor A School</p>
+                  <p className="c">
+                    Help an entire special needs or inclusive school experience
+                    the magic of a Zurura trip.{" "}
+                  </p>
+                </div>
+                <div className="c">
+                  <ArrowRight className="size-10" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/getInvolvedPage/HeroSection.tsx
@@ -0,0 +1,40 @@
+import { Button } from "../ui/button";
+
+export default function HeroSection() {
+  return (
+    <section className="min-h-screen w-full z-10">
+      <div className="flex ">
+        <div className="w-full min-h-screen bg-[url('https://res.cloudinary.com/dh8qlzbzk/image/upload/v1745753776/zurura-initiative_ActiviesBackground_Image_p0larw.png')] bg-cover bg-center"></div>
+        <div className="w-full grid place-content-center bg-lime-800 text-slate-100">
+          <div className="max-w-xl mx-auto space-y-6">
+            <h1 className="text-4xl font-bold">
+              It&apos;s Time to Take Action
+            </h1>
+            <p className="leading-8">
+              Thousands of children with special needs in Kenya miss out on
+              life-enriching experiences due to inaccessibility and lack of
+              resources. These children are often left behind—excluded from
+              opportunities that build confidence, spark curiosity, and create
+              joy.
+            </p>
+            <p className="leading-8">
+              But together, we can change that. With your support, we can open
+              doors to Kenya&apos;s wildlife, culture, and education for every
+              child—regardless of ability or background. There are many ways to
+              get involved.
+            </p>
+            <p className="leading-8">
+              What matters most is that we choose to act. Join us today—and help
+              create a world where every child gets to explore, learn, and
+              belong.
+            </p>
+
+            <Button className="bg-lime-600 w-48 h-12 hover:bg-lime-700 hover:scale-110 text-xl">
+              Donate
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
